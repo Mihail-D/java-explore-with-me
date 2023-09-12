4e81378a3c570cb82ffa1748001ae710aa8d5b8b
Nuby <nuby.4register@gmail.com>
2023-09-12 09:12:49 +0300

    stats-service client-main

---
+++ stats-service/client/src/main/java/ru/practicum/client/StatsServiceClient.java
@@ -0,0 +1,47 @@
+package ru.practicum.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.RequestHitInfoDto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class StatsServiceClient extends HttpRequestClient {
+    private static final String HIT_PREFIX = "/hit";
+    private static final String START_PREFIX = "/start";
+
+    @Autowired
+    public StatsServiceClient(@Value("${stats-service.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> getStatistic(String start, String end,
+                                               List<String> uris, Boolean unique) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("start", start);
+        parameters.put("end", end);
+        parameters.put("uris", String.join(",", uris));
+        parameters.put("unique", unique);
+
+        return get(START_PREFIX + "?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+    }
+
+    public ResponseEntity<Object> postEndpointHit(RequestHitInfoDto hitRequestDto) {
+        return post(HIT_PREFIX, hitRequestDto);
+    }
+}
