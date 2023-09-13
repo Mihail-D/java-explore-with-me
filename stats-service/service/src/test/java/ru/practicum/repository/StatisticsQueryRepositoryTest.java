ebea9446a1fd071c07c28ac889f633c453d4583b
Nuby <nuby.4register@gmail.com>
2023-09-13 12:51:15 +0300

    stats-service fixes

--- stats-service/service/src/test/java/ru/practicum/repository/StatisticsQueryRepositoryTest.java
+++ stats-service/service/src/test/java/ru/practicum/repository/StatisticsQueryRepositoryTest.java
@@ -1,11 +1,8 @@
 package ru.practicum.repository;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.practicum.model.RequestHitEntity;
